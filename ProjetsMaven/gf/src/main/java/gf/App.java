dd1602aa2ce9e5d6acdaaffac50d626379d4a419
GuillaumeFrogerSegret <ptitguigui41@hotmail.fr>
2017-04-09 19:14:46 +0200

    TD-Maven Exercice 9 Fini

---
+++ ProjetsMaven/gf/src/main/java/gf/App.java
@@ -0,0 +1,15 @@
+package gf;
+
+public class App 
+{
+    public static int max(int a, int b){
+		return a > b ? a : b; 
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "max(5,4) : "+max(5, 4) );
+    }
+    
+
+}
