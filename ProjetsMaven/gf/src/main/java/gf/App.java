d135bd233f63f236e9bfd6df22c8f28c91c91d1d
GuillaumeFrogerSegret <ptitguigui41@hotmail.fr>
2017-04-12 14:25:14 +0200

    Mise à jour du projet Maven et du rapport, I terminé

--- ProjetsMaven/gf/src/main/java/gf/App.java
+++ ProjetsMaven/gf/src/main/java/gf/App.java
@@ -3,7 +3,8 @@ package gf;
 public class App 
 {
     public static int max(int a, int b){
-		return a > b ? a : b; 
+		return a > b ? a : b;
+    	//return a; // Erreur volontaire
     }
     
     public static void main( String[] args )
